df33c9e3aba9c662f260641679353826b15dd45e
Pablo-Granados <pablogranados22@hotmail.com>
2023-03-09 19:24:48 -0300

    Clase 19 curso practico de React js

--- src/componentes/ProductItem.jsx
+++ src/componentes/ProductItem.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/ProductItem.scss';
 
+import agregar from "@icons/bt_add_to_cart.svg";
+
 const ProductItem = () => {
+
+	const [cart, setCart] = useState([]);
+
+	const handleClick = () => {
+		setCart("Hola mundo");
+	}
+
 	return (
 		<div className="ProductItem">
-			{/* <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" /> */}
+			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
 			<div className="product-info">
 				<div>
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick={handleClick}>
+					<img src={agregar} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,16 @@ module.exports = {
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			"@componentes": path.resolve(__dirname, "src/componentes/"),
+			"@containers": path.resolve(__dirname, "src/containers/"),
+			"@styles": path.resolve(__dirname, "src/styles/"),
+			"@icons": path.resolve(__dirname, "src/assets/icons/"),
+			"@logos": path.resolve(__dirname, "src/assets/logos/"),
+		}
+
+
+
 	},
 	module: {
 		rules: [
@@ -37,6 +47,10 @@ module.exports = {
 					"css-loader",
 					"sass-loader",
 				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: "asset"
 			}
 		]
 	},
