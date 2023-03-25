053698f1ac6b5801909cac110041e804fb75601e
Pablo-Granados <pablogranados22@hotmail.com>
2023-03-25 18:51:45 -0300

    añadido de home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Slider from '../componentes/Slider';
 
 const Home = () => {
+
 	return (
 		<div className="inicio">
 			<Slider />
-			<div>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente nemo culpa quasi neque voluptas molestiae, ipsa reiciendis aperiam corporis ea esse. Voluptatem amet molestiae sed iusto obcaecati, esse earum fuga?
+			<div className="contenedor-secciones-home">
+				<Link to={"graduaciones"}>
+					<div className="animate__animated animate__fadeInUp quinceañeras-home graduaciones">
+						<p className="quinceañeras-home-portada"> GRADUACIONES</p>
+					</div>
+				</Link>
+				<Link to={"quinces"}>
+					<div className="animate__animated animate__fadeInUp quinceañeras-home quinces">
+						<p className="quinceañeras-home-portada"> QUINCEAÑERAS</p>
+					</div>
+				</Link>
+				<div className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
+				<div className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
+				<div className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
+				<div className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
+				<div className="animate__animated animate__fadeInUp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
 			</div>
-			<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
-			<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
-			<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
-			<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
-			<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, amet. Nostrum accusamus sit illum iste temporibus quibusdam ipsum, qui natus recusandae unde architecto porro accusantium ipsa! Praesentium, ad. Recusandae, quae?</div>
 		</div>
 	);
 }
--- src/pages/Quinces.jsx
+++ src/pages/Quinces.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Quinces = () => {
   return (
     <div className="inicio">
-        <div className='algo'>
+        <div className="inicio-quinceañeras">
             <img src="https://i.postimg.cc/0NbgKhsz/Maria-391-1.webp" alt="vestido blanco" />
         </div>
     </div>
